445ff018def623820afc34608907586213a3f376
Manfred Endres <manfred.endres@wooga.net>
2022-07-07 21:15:38 +0200

    Split net.wooga.macos-security plugin
    Description
    ===========
    
    The `net.wooga.macos-security` plugin was split out of this repo.
    It resides in a new repo here: https://github.com/wooga/atlas-macos-security
    We split and merged content from this repo over so all commits will
    still be available but with different hashes.
    
    Changes
    =======
    
    * ![IMPROVE] split out plugin `net.wooga.macos-security`

--- src/integrationTest/groovy/wooga/gradle/macOS/security/tasks/InteractiveSecurityTaskIntegrationSpec.groovy
+++
@@ -1,8 +0,0 @@
-package wooga.gradle.macOS.security.tasks
-
-
-import wooga.gradle.macOS.security.SecurityTaskIntegrationSpec
-
-abstract class InteractiveSecurityTaskIntegrationSpec<T extends AbstractInteractiveSecurityTask> extends SecurityTaskIntegrationSpec<T> {
-    String keychainPassword = "123456"
-}
